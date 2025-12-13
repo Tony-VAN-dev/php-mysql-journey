99205eeb9861f6b238d57ad58d510d021fd743a9
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2025-12-13 22:20:53 +0100

    MAJ sur variables.php

--- module1/variables.php
+++ module1/variables.php
@@ -1,10 +1,10 @@
 <?php 
     $prenom = "Tony";
     $number = 20;
-    
-    echo "Je m'appelle ".$prenom;
-    echo "et j'ai ".$number." ans";
+    $nextYearAge = 0;
+    $nextYearAge += $number + 1;
+    echo "Je m'appelle ".$prenom." ";
+    echo "et j'ai ".$number."  <br>";
 
-    $number++;
-    echo "L'année prochaine j'aurai ".$number;
+    echo "L'année prochaine j'aurai ".$nextYearAge;
 ?>
---
+++ module2/index.php
@@ -0,0 +1,3 @@
+<?php
+    //les conditions
+?>
