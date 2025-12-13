192579a3f272a270bdddc1eef4b5e234f09e89df
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2025-12-13 22:49:28 +0100

    Pratique des conditions et de la boucle for

--- module2/index.php
+++ module2/index.php
@@ -1,3 +1,27 @@
 <?php
     //les conditions
+    $age = 0;
+
+    if($age < 18)
+    {
+        echo "Désolé, vous ne pouvez pas aller naviguer sur ce site <br>";
+    } else{
+        echo "Vous pouvez aller naviguer sur ce site, bonne journée / soirée à vous. <br>";
+    }
+
+    //tant que i est inférieur à 18 ou égale à 18, j'incrémente i
+    for($i = $age; $i <= 18 ;$i++)
+    {
+        echo "Vous avez ".$i." ans <br>";
+    }
+
+    $age = 18; // age vaut désormais 18
+    echo $age. "<br>";
+
+    if($age < 18)
+    {
+        echo "Désolé, vous ne pouvez pas aller naviguer sur ce site <br>";
+    } else{
+        echo "Vous pouvez aller naviguer sur ce site, bonne journée / soirée à vous. <br>";
+    }
 ?>
