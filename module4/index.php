046be1399b3a82485433b8c8991b4e955422476c
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2025-12-14 14:40:43 +0100

    Création de la fonction créer une personne

--- module4/index.php
+++ module4/index.php
@@ -1,20 +1,21 @@
 <?php
     require 'dbh.ext.php';
 
-    $sqlQuery = 'INSERT INTO personnes (prenom, age) VALUES("Ilyes", 20)';
-
-    if(mysqli_query($conn,$sqlQuery))
-    {
-        echo "Nouvelle personne ajoutée";
-    } else{
-        echo "Erreur : ".mysqli_error($conn);
-    }
     //si la requête fonctionne
     $sqlQuery = 'SELECT * FROM personnes;';
-    $result = mysqli_query($conn, $sqlQuery);
-    while($row = mysqli_fetch_assoc(result: $result))
-    {
-        echo "Prénom : ".$row['prenom'] . " " . "Age : " . $row['age']."<br>";
+
+    function creerUnePersonne($conn, $prenom,$age): void{
+        $createSomeoneQuery = 'INSERT INTO personnes (prenom, age) VALUES('. '"'. $prenom . '"'. ",".$age.");";
+        //condition si requête fonctionne
+        if(mysqli_query($conn, $createSomeoneQuery))
+            {
+                $result = mysqli_query($conn, "SELECT * FROM personnes".";");
+                while($row = mysqli_fetch_assoc(result: $result))
+                    {
+                        echo "prénom : ". $row['prenom']. " " ."âge" . $row['age'];
+                    }
+            }
     }
 
+    creerUnePersonne($conn, "Tony", 20);
 ?>
