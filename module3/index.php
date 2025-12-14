d549c3b43e700680b393be544bf8ee92872b6d1e
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2025-12-14 02:15:25 +0100

    Ajout formulaire et vérification si superglobale est vide, si non, afficher le prénom écrit dans le formulaire

--- module3/index.php
+++ module3/index.php
@@ -6,3 +6,13 @@
     echo direBonjour($prenom)."<br>";
 ?>
 
+<form method="post">
+    <input type="text" name="prenom" placeholder="Votre prénom">
+    <input type="submit" value="Envoyer">
+</form>
+
+<?php
+    if(isset($_POST['prenom'])){
+        echo direBonjour($_POST['prenom']);
+    }
+?>
